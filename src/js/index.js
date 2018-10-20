203966a57d15a0a72754d1cf5267a9c3e1b724db
Hobbytowo <weronika.ak@wp.pl>
2018-10-20 21:08:05 +0200

    add smooth scroll function

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,27 @@
 import $ from 'jquery'
+
+// START smooth scroll
+
+const smoothScroll = sectionTo => {
+  const hash = `#${ sectionTo }`
+
+  $('html, body').animate({
+    scrollTop: $(hash).offset().top
+  }, 600, () => {
+    if (sectionTo === 'home') {
+      window.location.hash = 0
+    } else {
+      window.location.hash = hash
+    }
+  })
+}
+
+$('.nav').on('click', e => {
+  smoothScroll(e.target.href.split('#')[1])
+})
+
+$('.header__btn').on('click', () => {
+  smoothScroll('portfolio')
+})
+
+// e/o smooth scroll
